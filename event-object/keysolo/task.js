aa9720a062970f30050728b02a14250a6263bf0f
AntonPnv <anton.panov992@gmail.com>
2023-08-15 16:29:27 +0400

    Задание с выпадающим списком выполнено

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+  	document.addEventListener('keydown', (event) => {
+  		const pressedKey = event.key.toLowerCase();
+
+  		if (this.currentSymbol.textContent.toLowerCase() === pressedKey) {
+  			this.success();
+  		} else {
+  			this.fail();
+  		}
+  	});
   }
 
   success() {
