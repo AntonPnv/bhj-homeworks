5b03521d3f4e8edeac743abeaf4853153bba5040
AntonPnv <anton.panov992@gmail.com>
2023-08-15 16:41:09 +0400

    Задание выпадающие списки выполнено

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,20 +1,20 @@
 const dropdowns = Array.from(document.querySelectorAll('.dropdown'));
 
 dropdowns.forEach(dropdown => {
-	const valueElement = dropdown.querySelector('.dropdown__value');
-	const listElement = dropdown.querySelector('.dropdown__list');
+  const valueElement = dropdown.querySelector('.dropdown__value');
+  const listElement = dropdown.querySelector('.dropdown__list');
 
-	valueElement.addEventListener('click', () => {
-		listElement.classList.toggle('dropdown__list_active');
-	});
+  valueElement.addEventListener('click', () => {
+      listElement.classList.toggle('dropdown__list_active');
+  });
 
-	listElement.addEventListener('click', event => {
-		event.preventDefault();
-		const dropdownItem = event.target.closest('.dropdown__item');
-		if (dropdownItem) {
-			const newValue = dropdownItem.textContent.trim();
-			valueElement.textContent = newValue;
-			listElement.classList.remove('dropdown__list_active');
-		}
-	});
+  listElement.addEventListener('click', event => {
+      event.preventDefault();
+      const dropdownItem = event.target.closest('.dropdown__item');
+      if (dropdownItem) {
+          const newValue = dropdownItem.textContent.trim();
+          valueElement.textContent = newValue;
+          listElement.classList.remove('dropdown__list_active');
+      }
+  });
 });
