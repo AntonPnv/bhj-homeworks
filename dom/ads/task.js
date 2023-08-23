3c3bcae7ab1756794a3cdf44c9e0f22118eb4e9a
AntonPnv <anton.panov992@gmail.com>
2023-08-23 11:08:50 +0400

    Устранены замечание

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const rotator = document.querySelectorAll('.rotator__case');
 	const defaultSpeed = 1000;
+	let index = 0;
 
-	rotator.forEach((element) => {
-		const speed = parseInt(element.dataset.speed) || defaultSpeed;
-		const color = element.dataset.color || 'black';
-		let index = 0;
+	const rotate = () => {
+		const prevIndex = index;
+		index= (index + 1) % rotator.length;
 
-		const rotate = () => {
-			rotator[index].classList.remove('rotator__case_active');
-			index = (index + 1) % rotator.length;
-			rotator[index].classList.add('rotator__case_active');
-			rotator[index].style.color = color;
-			setTimeout(rotate, speed);
-		}
+		rotator[prevIndex].classList.remove('rotator__case_active');
+		rotator[index].classList.add('rotator__case_active');
+
+		const speed = parseInt(rotator[index].dataset.speed) || defaultSpeed;
+		const color = rotator[index].dataset.color || 'black';
+		rotator[index].style.color = color;
 
 		setTimeout(rotate, speed);
-	});
+	}
+
+	rotate();
 });
